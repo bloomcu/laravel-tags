e37811b8201618758ba9e2fc40e83996a9529eaf
Ryan Harmon <heyharmon@gmail.com>
2021-10-24 20:58:03 -0600

    Add more tests

---
+++ tests/TagsCountTest.php
@@ -0,0 +1,94 @@
+<?php
+
+class TagsCountTest extends TestCase
+{
+    protected $lesson;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Create a lesson to work with
+        $this->lesson = \LessonStub::create([
+            'title' => 'A lesson title'
+        ]);
+    }
+
+    public function test_tag_count_is_incremented_when_tagged()
+    {
+        // Create a tag
+        $tag = \TagStub::create([
+            'name' => 'Red',
+            'slug' => Str::slug('Red'),
+            'count' => 0
+        ]);
+
+        // Tag the lesson
+        $this->lesson->tag(['red']);
+
+        // Get tag back from db
+        $tag = $tag->fresh();
+
+        // Assert tag count is 1
+        $this->assertEquals(1, $tag->count);
+    }
+
+    public function test_tag_count_is_decremented_when_untagged()
+    {
+        // Create a tag
+        $tag = \TagStub::create([
+            'name' => 'Red',
+            'slug' => Str::slug('Red'),
+            'count' => 100
+        ]);
+
+        // Tag the lesson, then untag it
+        $this->lesson->tag(['red']);
+        $this->lesson->untag(['red']);
+
+        // Get tag back from db
+        $tag = $tag->fresh();
+
+        // Assert tag count is 1
+        $this->assertEquals(100, $tag->count);
+    }
+
+    public function test_tag_count_cannot_be_less_than_zero()
+    {
+        // Create a tag
+        $tag = \TagStub::create([
+            'name' => 'Red',
+            'slug' => Str::slug('Red'),
+            'count' => 0
+        ]);
+
+        // Untag the lesson, despite not already being tagged
+        $this->lesson->untag(['red']);
+
+        // Get tag back from db
+        $tag = $tag->fresh();
+
+        // Assert tag count is still zero
+        $this->assertEquals(0, $tag->count);
+    }
+
+    public function test_tag_count_does_not_increment_if_tag_already_exists()
+    {
+        // Create a tag
+        $tag = \TagStub::create([
+            'name' => 'Red',
+            'slug' => Str::slug('Red'),
+            'count' => 0
+        ]);
+
+        // Tag lesson with same tag multiple times
+        $this->lesson->tag(['red']);
+        $this->lesson->retag(['red', 'white']);
+
+        // Get tag back from db
+        $tag = $tag->fresh();
+
+        // Assert red tag count is only 1
+        $this->assertEquals(1, $tag->count);
+    }
+}
--- tests/TagsTest.php
+++ tests/TagsTest.php
@@ -36,4 +36,74 @@ class TagsTest extends TestCase
             $this->assertContains($tag, $this->lesson->tags->pluck('name'));
         }
     }
+
+    public function test_can_untag_lesson()
+    {
+        // Tag lesson
+        $this->lesson->tag(['red', 'white', 'blue']);
+
+        // Untag lesson
+        $this->lesson->untag(['red', 'white']);
+
+        // Assert lesson has 1 tag
+        $this->assertCount(1, $this->lesson->tags);
+
+        // Assert lesson has the right tag
+        $this->assertContains('Blue', $this->lesson->tags->pluck('name'));
+    }
+
+    public function test_can_untag_all_lesson_tags()
+    {
+        // Tag lesson
+        $this->lesson->tag(['red', 'white', 'blue']);
+
+        // Untag lesson
+        $this->lesson->untagAll();
+
+        // Reload lesson
+        $this->lesson->load('tags');
+
+        // Assert lesson has 0 tags
+        $this->assertCount(0, $this->lesson->tags);
+    }
+
+    public function test_can_retag_lesson_tags()
+    {
+        // Tag lesson
+        $this->lesson->tag(['red', 'white', 'blue']);
+
+        // Retag, remove all tags, then add specific tags
+        $this->lesson->retag(['red', 'ash-grey']);
+
+        // Reload lesson
+        $this->lesson->load('tags');
+
+        // Assert lesson has the right tags
+        foreach (['Red', 'Ash Grey'] as $tag) {
+            $this->assertContains($tag, $this->lesson->tags->pluck('name'));
+        }
+    }
+
+    public function test_non_existing_tags_are_ignored_on_tagging()
+    {
+        // Tag lesson
+        $this->lesson->tag(['red', 'white', 'hamburger']);
+
+        // Assert lesson has only 2 tags
+        $this->assertCount(2, $this->lesson->tags);
+    }
+
+    public function test_random_tag_cases_are_normalised()
+    {
+        // Tag lesson
+        $this->lesson->tag(['Red', 'WHITE', 'bLuE', 'Ash-grey']);
+
+        // Assert lesson has 4 tags
+        $this->assertCount(4, $this->lesson->tags);
+
+        // Assert lesson has the right tags
+        foreach (['Red', 'White', 'Blue', 'Ash Grey'] as $tag) {
+            $this->assertContains($tag, $this->lesson->tags->pluck('name'));
+        }
+    }
 }
